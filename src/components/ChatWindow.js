4194645cffd445f02dbdaef8ca24921cbe01a898
Reuel Amaral <reuelsamaral@gmail.com>
2022-03-22 14:25:57 -0300

    Emoji system and speech recognition system for text completed.

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
 import './ChatWindow.css';
+
+import MessageItem from './MessageItem';
 
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
@@ -10,6 +13,52 @@ import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 
 export default () => {
+
+  let recognition = null;
+  let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  if(SpeechRecognition !== undefined) {
+    recognition = new SpeechRecognition();
+  }
+
+  const [emojiOpen, setEmojiOpen] = useState(false);
+  const [text, setText] = useState('');
+  const [listening, setListening] = useState(false);
+  const [list, setList] = useState([{}, {}, {}]);
+
+  const handleEmojiClick = (e, emojiObject) => {
+    setText( text + emojiObject.emoji );
+  }
+
+  const handleOpenEmoji =  () => {
+    setEmojiOpen(true);
+  }
+
+  const handleCloseEmoji =  () => {
+    setEmojiOpen(false);
+  }
+
+  const handleMicClick = () => {
+    if(recognition !== null ) {
+
+      recognition.onstart = () => {
+        setListening(true);
+      }
+      recognition.onend = () => {
+        setListening(false);
+      }
+      recognition.onresult = (e) => {
+        setText( e.results[0][0].transcript );
+      }
+
+      recognition.start();
+
+    }
+  }
+
+  const handleSendClick = () => {
+    
+  }
+
   return(
     <div className="chatWindow">
       <div className="chatWindow--header">
@@ -38,26 +87,66 @@ export default () => {
 
       </div>
       <div className="chatWindow--body">
-        
+        {list.map((item, key)=>(
+          <MessageItem 
+            key={key}
+            data={item}
+          />
+        ))}
       </div>
+
+      <div 
+      className="chatWindow--emojiarea" 
+      style={{height: emojiOpen ? '200px' : '0px'}}>
+        <EmojiPicker  
+          onEmojiClick={handleEmojiClick}
+          disableSearchBar
+          disableSkinTonePicker
+        />
+      </div>
+
       <div className="chatWindow--footer">
 
         <div className="chatWindow--pre">
-          <div className="chatWindow--btn">
-            <InsertEmoticonIcon style={{color: '#919191'}} />
+
+          <div 
+          className="chatWindow--btn"
+          onClick={handleCloseEmoji}
+          style={{width: emojiOpen ? '40px': '0px'}}
+          >
+            <CloseIcon style={{color: '#919191'}} />
+
+          </div>
+          <div 
+          className="chatWindow--btn" 
+          onClick={handleOpenEmoji}>
+            <InsertEmoticonIcon style={{color: emojiOpen ? '#009688' : '#919191'}} />
 
           </div>
         
         </div>
         <div className="chatWindow--inputarea">
-          <input className="chatWindow--input" type="text" placeholder="Digite uma mensagem"/>
+          <input 
+          className="chatWindow--input" 
+          type="text" 
+          placeholder="Digite uma mensagem"
+          value={text}
+          onChange={e=>setText(e.target.value)}
+          />
         
         </div>
         <div className="chatWindow--pos">
-         <div className="chatWindow--btn">
-            <SendIcon style={{color: '#919191'}} />
 
-          </div>
+          { text === '' &&
+            <div onClick={handleMicClick} className="chatWindow--btn">
+              <MicIcon style={{color: listening ? '#126ECE' : '#919191'}} />
+            </div>
+          } 
+          { text !== '' &&
+            <div onClick={handleSendClick} className="chatWindow--btn">
+              <SendIcon style={{color: '#919191'}} />
+            </div>
+          }
         </div>
 
       </div>
